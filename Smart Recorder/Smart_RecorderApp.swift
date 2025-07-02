0a8bb601db6e4270ceee13c40fba2eb99da80dc5
rohanpotta <pottarohan@gmail.com>
2025-07-02 13:03:07 -0500

    Initial commit

---
+++ Smart Recorder/Smart_RecorderApp.swift
@@ -0,0 +1,32 @@
+//
+//  Smart_RecorderApp.swift
+//  Smart Recorder
+//
+//  Created by Rohan Potta on 7/2/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Smart_RecorderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Smart RecorderTests/Smart_RecorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_RecorderTests.swift
+//  Smart RecorderTests
+//
+//  Created by Rohan Potta on 7/2/25.
+//
+
+import Testing
+@testable import Smart_Recorder
+
+struct Smart_RecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
